cb59dc276f4e423952d9fd33556c13a0f575f179
Shoaib Khan <shoaib7878687@gmail.com>
2026-04-14 14:08:43 +0500

    updated changes

--- @latest/src/Bmi.jsx
+++ @latest/src/Bmi.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
+import "./Bmi.css"
+
 export default function BMI(){
+
+    const [submit , setSubmit] = useState("")
+    const [reload , setReload] = useState("")
+
+    
     return(
-        <div>
+        <div className="App">
             <h1>BMI Calculator</h1>
+        <div className="Main">
+        <p>Weight (lbs)</p>
+        <input type="text" placeholder="Enter your weight" className="weight-field" />
+        <p>Height (in)</p>
+        <input type="text" placeholder="Enter your height" className="height-field" />
+        </div>
+        <div className="search">
+        <button>Submit</button>
+        </div>
+        <div className="reload-btn">
+        <button>Reload</button>
+        </div>
         </div>
     )
 }
