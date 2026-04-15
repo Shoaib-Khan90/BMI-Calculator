32cad8d2239206f8b96762a3a2a9225da1e53f48
Shoaib Khan <shoaib7878687@gmail.com>
2026-04-15 18:34:27 +0500

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import BMI from './Bmi'
+import BMI from './Bmi.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
